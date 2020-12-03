c8db53f76f0542c3a295e2f5a81d9c3917e627d1
Francis Duvivier <francisduvivier@gmail.com>
2020-12-03 09:29:13 +0100

    Leaderboard fetch working

--- src/aocLeaderBoardPoller.ts
+++ src/aocLeaderBoardPoller.ts
@@ -1,4 +1,5 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
+import fetch from "node-fetch";
 import * as config from "../config.json";
 
 export function getBoardUrl() {
@@ -9,10 +10,22 @@ export function getLastLeaderBoard() {
     return readFileSync('./data/aocleaderboard.json', { encoding: 'utf8' })
 }
 async function fetchLeaderBoard(): Promise<string> {
-    return readFileSync('./data/newleaderboard.json', { encoding: 'utf8' })
+    const requestInit = {
+        headers: {
+            'Cookie': config.aocCookie
+        }
+    };
+    const response = await fetch(getBoardUrl() + '.json', requestInit);
+    const responseText = await (response).text();
+    return responseText;
 }
 export async function getNewLeaderBoard() {
     const newLeaderBoardJson = await fetchLeaderBoard();
-    // fs.writeFileSync('./data/aocleaderboard.json', newLeaderBoardJson) // TODO
+    if (newLeaderBoardJson.startsWith('{') && JSON.parse(newLeaderBoardJson).members) {
+        writeFileSync(`./data/aocleaderboard-${Date.now()}.json`, getLastLeaderBoard());
+        writeFileSync('./data/aocleaderboard.json', newLeaderBoardJson);
+    } else {
+        throw Error('Bad leaderboard response:' + newLeaderBoardJson);
+    }
     return newLeaderBoardJson;
 }
