a7d2abed24f4b2f4ac93c4f4c73a83edb756e02d
Francis Duvivier <francisduvivier@gmail.com>
2020-12-04 09:01:37 +0100

    Filter unchanged members from message

--- src/bot.js
+++ src/bot.js
@@ -15,27 +15,29 @@ async function postToDiscordIfChanged() {
     console.log('Checking leaderboard now');
     const oldLeaderBoardJSON = getLastLeaderBoard();
     const newLeaderboardJSON = await getNewLeaderBoard();
-    if (leaderBoardChanged(oldLeaderBoardJSON, newLeaderboardJSON)) {
-        const description = createMessage(newLeaderboardJSON);
-        console.log('Old leaderboard!', createMessage(oldLeaderBoardJSON), getBoardUrl());
-        await sendMessage('Leaderboard Changed!', description, getBoardUrl());
+    const message = createMessage(newLeaderboardJSON, oldLeaderBoardJSON);
+    if (message.length) {
+        console.log('Old leaderboard!', createMemberLines(oldLeaderBoardJSON), getBoardUrl());
+        await sendMessage('Leaderboard Changed!', message, getBoardUrl());
     } else {
-        console.log('No change detected: ', createMessage(newLeaderboardJSON), getBoardUrl())
+        console.log('No change detected: ', createMemberLines(newLeaderboardJSON), getBoardUrl())
     }
 }
 
-function createMessage(leaderboardJSON) {
+function createMemberLines(leaderboardJSON) {
     const leaderboard = JSON.parse(leaderboardJSON);
     const membersObj = leaderboard.members;
     let members = membersObj && Object.getOwnPropertyNames(membersObj).map(key => membersObj[key]) || [];
     members.sort((m1, m2) => m2.local_score - m1.local_score);
-    return members.map(member => `${ member.name }: [${ member.stars }] stars, [${ member.local_score }] points`);
+    return members.map((member, index) => `**${ index + 1 }**: ${ member.name }: [**${ member.stars }**] stars, [**${ member.local_score }**] points`);
 }
 
-function leaderBoardChanged(oldLeaderBoardJSON, newLeaderboardJSON) {
-    const msgNew = createMessage(oldLeaderBoardJSON).join('\n');
-    const msgOld = createMessage(newLeaderboardJSON).join('\n');
-    return msgNew !== msgOld;
+function createMessage(newLeaderboardJSON, oldLeaderBoardJSON) {
+    const oldMemberLines = createMemberLines(oldLeaderBoardJSON);
+    const newMemberLines = createMemberLines(newLeaderboardJSON);
+    const changedLines = newMemberLines.filter((newLine) => oldMemberLines.indexOf(newLine) === -1);
+    return changedLines;
 }
+
 
 module.exports = { runBot };
