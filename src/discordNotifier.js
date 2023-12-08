54c9e965c1cdc0a5746706e7be2c526cfc7512a3
Francis Duvivier <fduvivier.contractor@libertyglobal.com>
2023-12-08 17:43:32 +0100

    Fix message sender for new discord.js version

--- src/discordNotifier.js
+++ src/discordNotifier.js
@@ -10,9 +10,10 @@ async function sendMessage(title, message, postFix) {
     if (message_with_cuttoff.length > DISCORD_CHAR_LIMIT - 6) { // 6 for 2x'```'
         message_with_cuttoff = message_with_cuttoff.substr(0, DISCORD_CHAR_LIMIT - 10) + '\n...';
     }
-    await webhookClient.send(title, {
+    await webhookClient.send({
+        content: title,
         username: 'AoC Leaderboard Monitor',
-        embeds: [ { description: '```' + message_with_cuttoff + '```' }, { description: postFix } ]
+        embeds: [ { title: 'Change List', description: '```' + message_with_cuttoff + '```' }, { description: postFix } ]
     });
 }
 
